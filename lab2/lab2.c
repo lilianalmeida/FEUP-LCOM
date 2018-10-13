f97e0612ac3b4ca0ae5f4394e840ce741b5fe073
José Gomes <44147511+JoeMGomes@users.noreply.github.com>
2018-10-13 17:59:50 +0000

    All inputs tested. Everything fully working

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -8,36 +8,42 @@
 
 #include "i8254.h"
 
-uint32_t counter =0;
+//global variable
+uint32_t counter = 0;
+
 
 int main(int argc, char *argv[]) {
-    // sets the language of LCF messages (can be either EN-US or PT-PT)
+  // sets the language of LCF messages (can be either EN-US or PT-PT)
   lcf_set_language("EN-US");
 
-    // enables to log function invocations that are being "wrapped" by LCF
-    // [comment this out if you don't want/need/ it]
+  // enables to log function invocations that are being "wrapped" by LCF
+  // [comment this out if you don't want/need/ it]
   lcf_trace_calls("/home/lcom/labs/lab2/trace.txt");
 
-    // enables to save the output of printf function calls on a file
-    // [comment this out if you don't want/need it]
+  // enables to save the output of printf function calls on a file
+  // [comment this out if you don't want/need it]
   lcf_log_output("/home/lcom/labs/lab2/output.txt");
 
-    // handles control over to LCF
-    // [LCF handles command line arguments and invokes the right function]
+  // handles control over to LCF
+  // [LCF handles command line arguments and invokes the right function]
   if (lcf_start(argc, argv))
     return 1;
 
-    // LCF clean up tasks
-    // [must be the last statement before return]
+  // LCF clean up tasks
+  // [must be the last statement before return]
   lcf_cleanup();
 
   return 0;
 }
 
 
-
 //// 7.1 ////
-int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
+int (timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
+
+  if (timer > 2 || timer < 0){
+    printf("Timer not found. Choose a timer between 0 and 2",0);
+    return 1;
+  }
 
   uint8_t st;
 
@@ -53,13 +59,17 @@ int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
     return erro;
   }
 
-  return OK;
+  return 0;
 }
 
 
-
 //// 7.2 ////
-int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
+int (timer_test_time_base)(uint8_t timer, uint32_t freq) {
+
+  if (timer > 2 || timer < 0){
+    printf("Timer not found",0);
+    return 1;
+  }
 
   int erro = timer_set_frequency(timer, freq);
   if (erro != OK) {
@@ -67,70 +77,76 @@ int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
     return erro;
   }
 
-  return OK;
+  return 0;
 }
 
 
-
 //// 7.3 ////
-int(timer_test_int)(uint8_t time) {
-  
-  uint8_t bit_no = 2;
+int (timer_test_int)(uint8_t time) {
 
-  timer_subscribe_int(&bit_no);
+  uint8_t bit_no = 1;
+
+  int erro=timer_subscribe_int(&bit_no);
+  if (erro != OK) {
+    printf("Error in timer_subscribe_int", 0);
+    return erro;
+  }
 
   int ipc_status;
   message msg;
-  unsigned int erro;
   uint32_t irq_set = bit_no;
 
-  while( counter/60 < time ) {
-     /* Get a request message. */
-    if ((erro = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
-     printf("driver_receive failed with: %d", erro);
+  while (counter / 60 < time) {
+    /* Get a request message. */
+    if ((erro = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("Driver_receive failed with: %d", erro);
       continue;
     }
 
     if (is_ipc_notify(ipc_status)) { /* received notification */
-      
+
       switch (_ENDPOINT_P(msg.m_source)) {
-      case HARDWARE: /* hardware interrupt notification */       
+      case HARDWARE: /* hardware interrupt notification */
         if (msg.m_notify.interrupts & irq_set) { /* subscribed interrupt */
-                    /* process it */
+          /* process it */
           timer_int_handler();
-          if(counter % 60 == 0){
-            timer_print_elapsed_time();}
-        }   
+          if (counter % 60 == 0) {
+            timer_print_elapsed_time();
+          }
+        }
         break;
-      
+
       default:
-        break; /* no other notifications expected: do nothing */ 
+        break; /* no other notifications expected: do nothing */
       }
 
     } else { /* received a standard message, not a notification */
-       /* no standard messages expected: do nothing */
+      /* no standard messages expected: do nothing */
     }
   }
 
-timer_unsubscribe_int();
+  erro=timer_unsubscribe_int();
+  if (erro != OK) {
+    printf("Error in timer_unsbscribe_int", 0);
+    return erro;
+  }
 
-return OK;
+  return 0;
 }
-
 
 
 //// 7.2 ////
-int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+int (util_get_LSB)(uint16_t val, uint8_t *lsb) {
 
-  *lsb = (uint8_t)val;   
+  *lsb = (uint8_t) val;
 
-  return OK;
+  return 0;
 }
 
-int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+int (util_get_MSB)(uint16_t val, uint8_t *msb) {
 
- val = val >> 8;
- *msb = (uint8_t)val;   
+  val = val >> 8;
+  *msb = (uint8_t) val;
 
- return OK;
+  return 0;
 }
--- lab2/timer.c
+++ lab2/timer.c
@@ -1,87 +1,83 @@
-	#include <lcom/lcf.h>
-	#include <lcom/timer.h>
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
 
-	#include <stdint.h>
+#include <stdint.h>
 
-	#include "i8254.h"
+#include "i8254.h"
 
-	//global variables
-	int hook_id = 0;
+//global variable
+int hook_id;
 
-	//// 7.2 ////
+
+//// 7.2 ////
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-
-	if (timer > 2 || timer < 0)
-		return 1;    
 
 	uint8_t control_byte;
 	uint8_t msb, lsb, st;
+    
+    if(freq >= TIMER_FREQ || freq < 19){
+        printf("Frequency not valid. ",0);
+        return 1;
+    }
 
-	      int erro = timer_get_conf(timer, &st); //read the timer configuration
-	      if (erro != OK) {
-	      	printf("Error in timer_get_conf", 0);
-	      	return erro;
-	      }
+	int erro = timer_get_conf(timer, &st); //read the timer configuration
+	if (erro != OK) {
+		printf("Error in timer_get_conf", 0);
+		return erro;
+	}
 
-		control_byte = (st & 0x0f) | (timer << 6) | TIMER_LSB_MSB; //control word to configure the timer
-		
-	      erro = sys_outb(TIMER_CTRL, control_byte); //write control word to configure timer
-	      if (erro != OK) {
-	      	printf("Error in sys_outb", 0);
-	      	return erro;
-	      }
+	control_byte = (st & 0x0f) | (timer << 6) | TIMER_LSB_MSB; //control word to configure the timer
 
+	erro = sys_outb(TIMER_CTRL, control_byte); //write control word to configure timer
+	if (erro != OK) {
+		printf("Error in sys_outb", 0);
+		return erro;
+	}
 
-	      uint16_t div = TIMER_FREQ/ freq;
+	uint16_t div = TIMER_FREQ / freq;
 
-	      util_get_LSB(div, &lsb);
-	      util_get_MSB(div, &msb);
+	util_get_LSB(div, &lsb);
+	util_get_MSB(div, &msb);
 
-	        erro = 	sys_outb(TIMER_0 + timer, lsb); //write lsb of the frequency desired first
-	        if (erro != OK) {
-	        	printf("Error in sys_outb", 0);
-	        	return erro;
-	        }
+	erro = sys_outb(TIMER_0 + timer, lsb); //write lsb of the frequency desired first
+	if (erro != OK) {
+		printf("Error in sys_outb", 0);
+		return erro;
+	}
 
-	        erro = 	sys_outb(TIMER_0 + timer, msb); //write msb of the frequency desired then
-	        if (erro != OK) {
-	        	printf("Error in sys_outb", 0);
-	        	return erro;
-	        }
+	erro = sys_outb(TIMER_0 + timer, msb); //write msb of the frequency desired then
+	if (erro != OK) {
+		printf("Error in sys_outb", 0);
+		return erro;
+	}
 
-	        return OK;
-	    }
-
+	return 0;
+}
 
 
 //// 7.3 ////
 int (timer_subscribe_int)(uint8_t * bit_no) {
 
-			*bit_no=BIT(hook_id);
+	*bit_no = BIT(hook_id);
 
-	    	int erro = sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id);
-	    	if (erro != OK) {
-	    		printf("Error in sys_irqsetpolicy", 0);
-	    		return erro;
-	    	}
-
-
-	return OK;
-
-}
-
-
-
-int (timer_unsubscribe_int)() {
-
-	int erro=sys_irqrmpolicy(&hook_id);
-	if (erro != OK){
-		printf("Error in sys_irqrmpolicy",0);
+	int erro = sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id);
+	if (erro != OK) {
+		printf("Error in sys_irqsetpolicy", 0);
 		return erro;
 	}
 
+	return 0;
+}
 
-	return OK;
+int (timer_unsubscribe_int)() {
+
+	int erro = sys_irqrmpolicy(&hook_id);
+	if (erro != OK) {
+		printf("Error in sys_irqrmpolicy", 0);
+		return erro;
+	}
+
+	return 0;
 }
 
 void (timer_int_handler)() {
@@ -89,69 +85,64 @@ void (timer_int_handler)() {
 }
 
 
-
-	//// 7.1 ////
+//// 7.1 ////
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
-	if (timer > 2 || timer < 0)
-		return 1;
+	uint32_t rbc = TIMER_RB_CMD | TIMER_RB_SEL(timer) | TIMER_RB_COUNT_; //read back command
 
-		uint32_t rbc = TIMER_RB_CMD | TIMER_RB_SEL(timer) | TIMER_RB_COUNT_; //read back command
-
-		int erro = sys_outb(TIMER_CTRL, rbc);
-		if (erro != OK) {
-			printf("error in sys_outb");
-			return erro;
-		}
-
-		uint32_t st32; //
-
-		erro = sys_inb(TIMER_0 + timer, &st32);
-		if (erro != OK) {
-			printf("error in sys_inb");
-			return erro;
-		}
-
-		*st = (uint8_t) st32;
-
-		return OK;
+	int erro = sys_outb(TIMER_CTRL, rbc);
+	if (erro != OK) {
+		printf("Error in sys_outb");
+		return erro;
 	}
 
+	uint32_t st32; //
 
-	int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
+	erro = sys_inb(TIMER_0 + timer, &st32);
+	if (erro != OK) {
+		printf("Error in sys_inb");
+		return erro;
+	}
 
-		union timer_status_field_val uni;
-		
-		switch(field)
-		{
-			case all:
-			uni.byte = st;
-			break;
+	*st = (uint8_t) st32;
 
-			case initial:
-			uni.in_mode = (st & (BIT(5) | BIT(4))) >> 4;
-			break;
+	return 0;
+}
 
-			case mode:
-			uni.count_mode = (st & (BIT(3) | BIT(2) | BIT(1))) >> 1;
-	            if (uni.count_mode == 0x06) //when 110 -> operating mode is 2
-	            	uni.count_mode = 0x02;
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
+    
+	union timer_status_field_val uni;
 
-	            if(uni.count_mode == 0x07) //when 111 -> operating mode is 3
-	            	uni.count_mode = 0x03;
-	            break;
+	switch (field) {
+	case all:
+		uni.byte = st;
+		break;
 
-	            case base:
-	            uni.bcd = st & BIT(0);
-	            break;
-	        }	
+	case initial:
+		uni.in_mode = (st & (BIT(5) | BIT(4))) >> 4;
+		break;
 
-	        int erro = timer_print_config(timer, field, uni);
-	        if (erro != OK) {
-	        	printf("error in print_config");
-	        	return erro;
-	        }
+	case mode:
+		uni.count_mode = (st & (BIT(3) | BIT(2) | BIT(1))) >> 1;
+		if (uni.count_mode == 0x06) //when 110 -> operating mode is 2
+			uni.count_mode = 0x02;
 
+		if (uni.count_mode == 0x07) //when 111 -> operating mode is 3
+			uni.count_mode = 0x03;
+		break;
 
-	        return OK;
-	    }
+	case base:
+		uni.bcd = st & BIT(0);
+		break;
+    default:
+        printf("timer_status_field not recognized",0);
+	}
+
+	int erro = timer_print_config(timer, field, uni);
+	if (erro != OK) {
+		printf("Error in print_config");
+		return erro;
+	}
+
+	return 0;
+}
