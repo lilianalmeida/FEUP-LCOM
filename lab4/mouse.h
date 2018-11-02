08c4b2d04626d60b7aba8d67f2a49c52dd46cb91
up201706908 <up201706908@d120e3e7-3006-4f0b-a616-746b5f7a7f47>
2018-11-02 23:40:19 +0000

    svn commit --username up201706908 -m

--- lab4/mouse.h
+++ lab4/mouse.h
@@ -2,12 +2,12 @@
 #define _MOUSE_H_
 
 
-int (mouse_subscribe)(int * mouse_id);
-int (mouse_unsubscribe)(int * mouse_id);
+int (mouse_subscribe)(uint8_t * bit_no);
+int (mouse_unsubscribe)();
 int (mouse_enable)();
 int (mouse_disable)();
 int (write_kbc)(uint32_t cmd_byte);
 int (OB_cleaner)();
 int (mouse_handler)(uint32_t *mouse_packet);
 
-#endif
+#endif //_MOUSE_H_
