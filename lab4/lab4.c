1215c9c03b07c70ad20717d90baa040355e2b81c
up201706908 <up201706908@d120e3e7-3006-4f0b-a616-746b5f7a7f47>
2018-11-09 19:57:28 +0000

    'mouse_test_gesture' begined and added time files

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -4,7 +4,7 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include "mouse.h"
+#include "mouse_test.h"
 #include "macros.h"
 #include "i8254.h"
 
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
   // handles control over to LCF
   // [LCF handles command line arguments and invokes the right function]
   if (lcf_start(argc, argv))
-    return 1;
+  return 1;
 
   // LCF clean up tasks
   // [must be the last statement before return]
@@ -38,6 +38,7 @@ int main(int argc, char *argv[]) {
 int (mouse_test_packet)(uint32_t cnt) {
   int ipc_status;
   uint32_t counter = 0;
+  struct packet pp;
 
 
   message msg;
@@ -48,123 +49,123 @@ int (mouse_test_packet)(uint32_t cnt) {
     printf("The program failed to enable the mouse data reporting\n");
     return 1;
   }
- // mouse_enable_data_reporting();
+  //mouse_enable_data_reporting();
 
   if(mouse_subscribe(&mouse_id) != 0){
-  printf("Error subscribing mouse notifications\n");
-  return -1;
+    printf("Error subscribing mouse notifications\n");
+    return -1;
   }
   uint32_t irq_set = BIT(mouse_id);
 
-OB_cleaner(); // Clear the output buffer
+  OB_cleaner(); // Clear the output buffer
 
-while(counter < cnt) {
+  while(counter < cnt) {
 
     if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
-        printf("driver_receive failed with: %d\n", r);
-        continue;
+      printf("driver_receive failed with: %d\n", r);
+      continue;
     }
 
     if (is_ipc_notify(ipc_status)) { /* received notification */
-        switch (_ENDPOINT_P(msg.m_source)) {
-            case HARDWARE: /* hardware interrupt notification */
-                if (msg.m_notify.interrupts & irq_set) { /* subscribed interrupt */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: /* hardware interrupt notification */
+        if (msg.m_notify.interrupts & irq_set) { /* subscribed interrupt */
 
-                  /////// Parte a rever ///////////
-                  mouse_ih();
+          /////// Parte a rever ///////////
+          mouse_ih();
 
-                  if (kbc_ih_error == true){
-                    byteNumber = 0;
-                    continue;
-                  }
+          if (kbc_ih_error == true){
+            byteNumber = 0;
+            continue;
+          }
 
-                  if(byteNumber == 3){
-                    counter++;
-                    print_packet();
-                    byteNumber = 0;
-                  }
-                  }
-                  ////// Até aqui ////////////
-
-                break;
-            default:
-                break; /* no other notifications expected: do nothing */
+          if(byteNumber == 3){
+            counter++;
+            print_packet(&pp);
+            byteNumber = 0;
+          }
         }
+        ////// Até aqui ////////////
+
+        break;
+        default:
+        break; /* no other notifications expected: do nothing */
+      }
     } else { /* received a standard message, not a notification */
-        /* no standard messages expected: do nothing */
+      /* no standard messages expected: do nothing */
     }
- }
+  }
 
-if(mouse_unsubscribe() != 0){
-      printf("The program was unable to unsubscribe a mouse notification\n");
-      return 1;
-    }
+  if(mouse_unsubscribe() != 0){
+    printf("The program was unable to unsubscribe a mouse notification\n");
+    return 1;
+  }
 
-    if(mouse_disable() != 0){
-  printf("Error disabling mouse data reporting\n");
-  return 1;
-}
+  if(mouse_disable() != 0){
+    printf("Error disabling mouse data reporting\n");
+    return 1;
+  }
 
-OB_cleaner(); // Clear the output buffer
-return 0;
+  OB_cleaner(); // Clear the output buffer
+  return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 int (mouse_test_remote)(uint16_t period, uint8_t cnt) {
+ struct packet pp;
+ uint32_t counter = 0;
 
-uint32_t counter = 0;
+  if(disable_mouse_interrupts() != OK){
+    return 1;
+  }
 
-    if(disable_mouse_interrupts() != OK){
-      return 1;
+  if(mouse_disable() != OK){
+    return 1;
+  }
+
+  if(set_remote_mode() != OK){
+    return 1;
+  }
+  OB_cleaner();
+
+  while (counter < cnt){
+    //printf("Counter: %x\n", counter);
+    if (byteNumber == 0){
+      write_kbc(READ_DATA);
     }
 
-    if(mouse_disable() != OK){
-      return 1;
+    mouse_ih();
+
+    if (kbc_ih_error == true){
+      byteNumber = 0;
+      continue;
     }
 
-    if(set_remote_mode() != OK){
-      return 1;
-    }
-    OB_cleaner();
-
-    while (counter < cnt){
-    	//printf("Counter: %x\n", counter);
-    	if (byteNumber == 0){
-    		write_kbc(READ_DATA);
-    	}
-
-      	mouse_ih();
-
-      if (kbc_ih_error == true){
-          byteNumber = 0;
-          continue;
-      }
-
-      if(byteNumber == 3){
-        print_packet();
-        byteNumber = 0;
-        counter++;
-        tickdelay (micros_to_ticks(period*1000));
-      }
-
+    if(byteNumber == 3){
+      print_packet(&pp);
+      byteNumber = 0;
+      counter++;
+      tickdelay (micros_to_ticks(period*1000));
     }
 
-    if(mouse_disable() != OK){
-      return 1;
-    }
+  }
 
-    if(set_stream_mode() != OK){
-      return 1;
-    }
+  if(mouse_disable() != OK){
+    return 1;
+  }
 
-    if(enable_mouse_interrupts()!= OK){
-      return 1;
-    }
-OB_cleaner();
+  if(set_stream_mode() != OK){
+    return 1;
+  }
 
-    return 0;
+  if(enable_mouse_interrupts()!= OK){
+    return 1;
+  }
+  OB_cleaner();
+
+  return 0;
 }
 
 
@@ -175,18 +176,22 @@ int (mouse_test_async)(uint8_t idle_time) {
 
   uint8_t bit_no_mouse, bit_no_timer;
   uint32_t erro;
+  struct packet pp;
 
-
-if (timer_subscribe_int(&bit_no_timer)) { //subscribes the timer
+  if (timer_subscribe_int(&bit_no_timer)) { //subscribes the timer
     printf("Error in timer_subscribe_int", 0);
     return 1;
   }
 
-  mouse_enable_data_reporting();
+ if(mouse_enable() != 0){
+    printf("The program failed to enable the mouse data reporting\n");
+    return 1;
+  }
+  //mouse_enable_data_reporting();
 
   if(mouse_subscribe(&bit_no_mouse) != 0){
-  printf("Error subscribing mouse notifications\n");
-  return 1;
+    printf("Error subscribing mouse notifications\n");
+    return 1;
   }
 
   uint32_t irq_set_mouse = BIT(bit_no_mouse);
@@ -197,7 +202,7 @@ if (timer_subscribe_int(&bit_no_timer)) { //subscribes the timer
 
   while (counter_t / 60 < idle_time) { //Stops if ESC is pressed or has no input for n seconds
     /* Get a request message. */
-  	 //printf("Counter %x\n", counter_t);
+    //printf("Counter %x\n", counter_t);
 
     if ((erro = driver_receive(ANY, &msg, &ipc_status)) != 0) {
       printf("Driver_receive failed with: %d", erro);
@@ -207,50 +212,50 @@ if (timer_subscribe_int(&bit_no_timer)) { //subscribes the timer
     if (is_ipc_notify(ipc_status)) { /* received notification */
 
       switch (_ENDPOINT_P(msg.m_source)) {
-      case HARDWARE: /* hardware interrupt notification */
+        case HARDWARE: /* hardware interrupt notification */
         if (msg.m_notify.interrupts & irq_set_timer) {/* subscribed interrupt */
           timer_int_handler();
         }
         if (msg.m_notify.interrupts & irq_set_mouse) { /* subscribed interrupt */
-   			mouse_ih();
+          mouse_ih();
 
-            if (kbc_ih_error == true){
-                byteNumber = 0;
-                continue;
-            }
+          if (kbc_ih_error == true){
+            byteNumber = 0;
+            continue;
+          }
 
-            if(byteNumber == 3){
-                print_packet();
-                byteNumber = 0;
-                counter_t = 0;
-            }
-   		}
-
-          break;
-
-          default:
-          break; /* no other notifications expected: do nothing */
+          if(byteNumber == 3){
+            print_packet(&pp);
+            byteNumber = 0;
+            counter_t = 0;
+          }
         }
 
-      }
-      else { /* received a standard message, not a notification */
-        /* no standard messages expected: do nothing */
-      }
-    }
+        break;
 
-if ( timer_unsubscribe_int()) { //unsibscribes the timer
+        default:
+        break; /* no other notifications expected: do nothing */
+      }
+
+    }
+    else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+  }
+
+  if ( timer_unsubscribe_int()) { //unsibscribes the timer
     printf("Error in timer_unsbscribe_int", 0);
     return 1;
   }
- if(mouse_unsubscribe() != 0){
-      printf("The program was unable to unsubscribe a mouse notification\n");
-      return 1;
-    }
+  if(mouse_unsubscribe() != 0){
+    printf("The program was unable to unsubscribe a mouse notification\n");
+    return 1;
+  }
 
-    if(mouse_disable() != 0){
-  printf("Error disabling mouse data reporting\n");
-  return 1;
-}
+  if(mouse_disable() != 0){
+    printf("Error disabling mouse data reporting\n");
+    return 1;
+  }
 
   OB_cleaner();
 
@@ -260,8 +265,159 @@ if ( timer_unsubscribe_int()) { //unsibscribes the timer
 ///////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-int (mouse_test_gesture)(uint8_t UNUSED(x_len), uint8_t UNUSED(tolerance)) {
-    /* To be completed */
-    printf("%s: under construction\n", __func__);
+int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance){
+
+  int ipc_status;
+  message msg;
+  uint8_t mouse_id;
+  uint32_t r;
+  struct packet pp;
+  struct mouse_ev event;
+  event.delta_x = 0;
+  event.delta_y = 0;
+
+
+  /*if(mouse_enable() != 0){
+    printf("The program failed to enable the mouse data reporting\n");
     return 1;
+  }
+*/
+  mouse_enable_data_reporting();
+
+  if(mouse_subscribe(&mouse_id) != 0){
+    printf("Error subscribing mouse notifications\n");
+    return -1;
+  }
+  uint32_t irq_set = BIT(mouse_id);
+
+  OB_cleaner(); // Clear the output buffer
+
+  while(state != COMP) {
+
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+      printf("driver_receive failed with: %d\n", r);
+      continue;
+    }
+
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: /* hardware interrupt notification */
+        if (msg.m_notify.interrupts & irq_set) { /* subscribed interrupt */
+
+          mouse_ih();
+
+          if (kbc_ih_error == true){
+            byteNumber = 0;
+            continue;
+          }
+            if(byteNumber == 3){
+              print_packet(&pp);
+              byteNumber = 0;
+                printf("ola %x\n", state);
+              
+              if (!pp.rb && pp.lb && !pp.mb){ //if left button is pressed
+                if (state == INIT || state == VERTEX){
+                  event.type = LB_PRESSED;
+                gesture_handler(&event, x_len); continue;}
+                printf("%x\n", state);
+                event.delta_x += pp.delta_x;
+                event.delta_y += pp.delta_y;
+                if (pp.delta_x > -tolerance && pp.delta_y > -tolerance){ //displacements in x and y
+                  if (event.delta_y / event.delta_x > 0){ //positive slope
+                    event.type = MOUSE_MOV;
+                    printf("%x\n", state);
+                    gesture_handler(&event, x_len);
+                  }
+                  else{
+                    event.type = BUTTON_EV;
+                    gesture_handler(&event, x_len);
+                    printf("%x\n", state);
+                  }
+                }else{
+                  event.type = BUTTON_EV;
+                  gesture_handler(&event, x_len);
+                   printf("%x\n", state);
+                }
+              }else if (pp.rb && !pp.lb && !pp.mb){ //if rigth button is pressed
+                if(state == VERTEX || state == VERTEX){
+                  event.type = RB_PRESSED;
+                gesture_handler(&event, x_len);
+                continue;
+              }
+              printf("%x\n", state);
+                event.delta_x += pp.delta_x;
+                event.delta_y += pp.delta_y;
+                if (pp.delta_x > tolerance && pp.delta_y > tolerance){ //displacements in x and y
+                  if (event.delta_y / event.delta_x < 0){ //positive slope
+                    event.type = MOUSE_MOV;
+                    gesture_handler(&event, x_len);
+                     printf("%x\n", state);
+                  }
+                  else{
+                    event.type = BUTTON_EV;
+                    gesture_handler(&event, x_len);
+                     printf("%x\n", state);
+                  }
+                }else{
+                  event.type = BUTTON_EV;
+                  gesture_handler(&event, x_len);
+                   printf("%x\n", state);
+                }
+
+              }else if (!pp.rb && !pp.lb && !pp.mb){ //if no button is pressed
+                if (state == LINE1 || state == DRAW1){
+                  event.type = LB_RELEASED;
+                }else if (state == LINE2 || state == DRAW2){
+                  event.type = RB_RELEASED;
+                }
+                else if(state == VERTEX) {
+                  if (abs(pp.delta_x) <= tolerance && abs(pp.delta_y) <= tolerance){
+                    event.type = MOUSE_MOV;
+                  }
+                  else{
+                    event.type = BUTTON_EV;
+                  }
+
+                }
+                else
+                event.type = BUTTON_EV;
+
+                gesture_handler(&event, x_len);
+                printf("%x\n", state);
+              }
+
+
+              else{ //if the middle button is pressed or more than one button is pressed
+                event.type = BUTTON_EV;
+                gesture_handler(&event, x_len);
+                printf("%x\n", state);
+              }
+
+
+
+
+          }
+
+        }
+        break;
+        default:
+        break; /* no other notifications expected: do nothing */
+      }
+    } else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+  }
+
+  if(mouse_unsubscribe() != 0){
+    printf("The program was unable to unsubscribe a mouse notification\n");
+    return 1;
+  }
+
+  if(mouse_disable() != 0){
+    printf("Error disabling mouse data reporting\n");
+    return 1;
+  }
+
+  OB_cleaner(); // Clear the output buffer
+  return 0;
 }
---
+++ lab4/timer.c
@@ -0,0 +1,148 @@
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
+
+#include <stdint.h>
+
+#include "i8254.h"
+#include "macros.h"
+
+//global variable
+int hook_id_t = 0x02;
+
+//// 7.2 ////
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+
+	uint8_t control_byte;
+	uint8_t msb, lsb, st;
+
+    if(freq >= TIMER_FREQ || freq < 19){
+        printf("Frequency not valid. ",0);
+        return 1;
+    }
+
+	int erro = timer_get_conf(timer, &st); //read the timer configuration
+	if (erro != OK) {
+		printf("Error in timer_get_conf", 0);
+		return erro;
+	}
+
+	control_byte = (st & 0x0f) | (timer << 6) | TIMER_LSB_MSB; //control word to configure the timer
+
+	erro = sys_outb(TIMER_CTRL, control_byte); //write control word to configure timer
+	if (erro != OK) {
+		printf("Error in sys_outb", 0);
+		return erro;
+	}
+
+	uint16_t div = TIMER_FREQ / freq;
+
+	util_get_LSB(div, &lsb);
+	util_get_MSB(div, &msb);
+
+	erro = sys_outb(TIMER_0 + timer, lsb); //write lsb of the frequency desired first
+	if (erro != OK) {
+		printf("Error in sys_outb", 0);
+		return erro;
+	}
+
+	erro = sys_outb(TIMER_0 + timer, msb); //write msb of the frequency desired then
+	if (erro != OK) {
+		printf("Error in sys_outb", 0);
+		return erro;
+	}
+
+	return 0;
+}
+
+
+//// 7.3 ////
+int (timer_subscribe_int)(uint8_t * bit_no) {
+
+	*bit_no = hook_id_t;
+
+	int erro = sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id_t);
+	if (erro != OK) {
+		printf("Error in sys_irqsetpolicy", 0);
+		return erro;
+	}
+
+	return 0;
+}
+
+int (timer_unsubscribe_int)() {
+
+	int erro = sys_irqrmpolicy(&hook_id_t);
+	if (erro != OK) {
+		printf("Error in sys_irqrmpolicy", 0);
+		return erro;
+	}
+
+	return 0;
+}
+
+void (timer_int_handler)() {
+	counter_t++;
+}
+
+
+//// 7.1 ////
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+
+	uint32_t rbc = TIMER_RB_CMD | TIMER_RB_SEL(timer) | TIMER_RB_COUNT_; //read back command
+
+	int erro = sys_outb(TIMER_CTRL, rbc);
+	if (erro != OK) {
+		printf("Error in sys_outb");
+		return erro;
+	}
+
+	uint32_t st32; //
+
+	erro = sys_inb(TIMER_0 + timer, &st32);
+	if (erro != OK) {
+		printf("Error in sys_inb");
+		return erro;
+	}
+
+	*st = (uint8_t) st32;
+
+	return 0;
+}
+
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
+
+	union timer_status_field_val uni;
+
+	switch (field) {
+	case all:
+		uni.byte = st;
+		break;
+
+	case initial:
+		uni.in_mode = (st & (BIT(5) | BIT(4))) >> 4;
+		break;
+
+	case mode:
+		uni.count_mode = (st & (BIT(3) | BIT(2) | BIT(1))) >> 1;
+		if (uni.count_mode == 0x06) //when 110 -> operating mode is 2
+			uni.count_mode = 0x02;
+
+		if (uni.count_mode == 0x07) //when 111 -> operating mode is 3
+			uni.count_mode = 0x03;
+		break;
+
+	case base:
+		uni.bcd = st & BIT(0);
+		break;
+    default:
+        printf("timer_status_field not recognized",0);
+	}
+
+	int erro = timer_print_config(timer, field, uni);
+	if (erro != OK) {
+		printf("Error in print_config");
+		return erro;
+	}
+
+	return 0;
+}
