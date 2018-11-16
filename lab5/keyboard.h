2c4e8bccfb790813e4e5c8fd4c3da000d6f487e2
José Gomes <44147511+JoeMGomes@users.noreply.github.com>
2018-11-16 02:29:55 +0000

    Implemented video_test_init() and added keyboard files for the rectangle function

---
+++ lab5/keyboard.h
@@ -0,0 +1,26 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "macro.h"
+#include "i8254.h"
+
+
+#ifdef LAB3
+int sys_inb_count(port_t port, uint32_t *byte);
+#else
+#define sys_inb_cnt(p,q) sys_inb(p,q)
+#endif
+
+//global variable
+extern uint32_t counter; //sys_inb counter
+extern bool kbc_ih_error;
+
+//int sys_inb_count (port_t port, uint32_t *byte);
+int (keyboard_subscribe)(uint8_t * bit_no);
+int (keyboard_unsubscribe)();
+int (kbc_pol)();
+int (scancode_parse)(uint32_t byte, uint8_t nbyte);
+int (interrupt_handler)();
+void (isTwoByte)(bool *wait, uint8_t *nbyte);
