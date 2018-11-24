a7784d4e7d849d53dc1627bd49fff65693cd89ed
up201706908 <up201706908@d120e3e7-3006-4f0b-a616-746b5f7a7f47>
2018-11-24 20:08:24 +0000

    'video_test_move' done and minor fixes

--- lab5/video_gr.h
+++ lab5/video_gr.h
@@ -1,11 +1,16 @@
 #ifndef _VIDEO_GR_H_
 #define _VIDEO_GR_H_
 
-//int (vg_initialize)(uint16_t mode);
-int (col)(uint16_t x, uint16_t y, uint32_t color);
-//int (drawRect)(uint16_t x,uint16_t y,uint16_t width, uint16_t height, uint32_t color);
-int (drawPattern) (uint8_t no_rectangles, uint32_t first, uint8_t step);
-//int vg_draw_hline	(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
-int (drawSprite) (const char *xpm[], uint16_t x, uint16_t y);
+#include "sprite.h"
+
+int vbe_get_mode_inf(uint16_t mode, vbe_mode_info_t* vmi_p);
+int col(uint16_t x, uint16_t y, uint32_t color);
+int drawPattern (uint8_t no_rectangles, uint32_t first, uint8_t step);
+int drawSprite (const char *xpm[], uint16_t x, uint16_t y);
+void draw_sprite(Sprite *sp);
+void erase_sprite(Sprite *sp);
+int vbe_get_controller_info();
+
+void move_sprite(Sprite *sprite, uint16_t xi, uint16_t yi, uint16_t xf, uint16_t yf, int16_t speed);
 
 #endif
