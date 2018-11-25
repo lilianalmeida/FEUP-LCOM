7e945841296d641f8abfd8262d26e0e7e49bf971
Liliana Almeida <44161002+lilianalmeida@users.noreply.github.com>
2018-11-25 19:28:47 +0000

    deleted unecessary files

--- lab5/vbe_test.h
+++
@@ -1,7 +0,0 @@
-#ifndef _VBE_TEST_H_
-#define _VBE_TEST_H_
-
-int (col)(uint16_t x, uint16_t y, uint32_t color);
-int (drawSquare)(uint16_t x,uint16_t y,uint16_t width, uint16_t height, uint32_t color);
-
-#endif
