bbded529a2aac075d07a23f72e295dc80f406c3f
up201707054 <up201707054@d120e3e7-3006-4f0b-a616-746b5f7a7f47>
2018-12-07 23:11:13 +0000

    Added Main Menu and organized files system

--- proj/video_gr.h
+++ proj/video_gr.h
@@ -6,7 +6,7 @@
 int vbe_get_mode_inf(uint16_t mode, vbe_mode_info_t* vmi_p);
 int col(uint16_t x, uint16_t y, uint32_t color);
 int drawPattern (uint8_t no_rectangles, uint32_t first, uint8_t step);
-int drawSprite (const char *xpm[], uint16_t x, uint16_t y);
+//int drawSprite (const char *xpm[], uint16_t x, uint16_t y);
 //void draw_sprite(Sprite *sp);
 //void erase_sprite(Sprite *sp);
 int vbe_get_controller_info();
