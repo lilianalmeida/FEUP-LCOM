c385c6ff267e4789404fa233ae71df61a5f011c8
Liliana Almeida <44161002+lilianalmeida@users.noreply.github.com>
2018-12-18 11:07:10 +0000

    Updated movement with keyboard

--- proj/game.h
+++ proj/game.h
@@ -2,7 +2,7 @@
 #define _GAME_H_
 
 void movePlayer(Sprite* sp);
-void set_move(Sprite *sp);
+void set_move(Sprite *sp, uint8_t nbyte);
 void throwBall(Sprite* ball);
 void initGame();
 void shootBall(Sprite* ball, Sprite* aim);
