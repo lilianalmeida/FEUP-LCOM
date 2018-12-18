787d08abb7f457bc8b37f6a3657d95e89b5fe1c5
Liliana Almeida <44161002+lilianalmeida@users.noreply.github.com>
2018-12-18 11:18:49 +0000

    Updated movement with keyboard

--- proj/main_menu.h
+++ proj/main_menu.h
@@ -4,7 +4,7 @@
 typedef enum {SINGLE_PL, MULTI_PL, HIGHSCORE, EXIT} menu_options;
 
 void start_main_menu ();
-void parse_kbc_keys(Sprite *selector, Bitmap *menu_back);
+void parse_kbc_keys(Sprite *selector, Bitmap *menu_back, uint8_t nbyte);
 void call_func_menu();
 
 
