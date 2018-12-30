69ed1d8e877e68e49c8d92952892ef9d42ff4465
José Gomes <44147511+JoeMGomes@users.noreply.github.com>
2018-12-30 01:30:40 +0000

    Player movement sent through serial_port

--- proj/game.c
+++ proj/game.c
@@ -73,8 +73,7 @@ void singlePlayerGame() {
           if (wait == false) {
             scancode_parse(scanByte, nbyte);
           }
-          set_move(player, nbyte);
-
+          set_move(player, nbyte, 0);
           tickdelay(micros_to_ticks(DELAY_US));
         }
         if (msg.m_notify.interrupts & timer_irq_set) {
@@ -98,7 +97,7 @@ void singlePlayerGame() {
             }
             ball->x += ball->xspeed;
             ball->y += ball->yspeed;
-            movePlayer(player);
+            movePlayer(player,0, (uint32_t)getHorResolution()/2);
             if(ball->colided) {
               if (is_left_pressed()){
                 shootBall(ball, aim);
@@ -148,20 +147,296 @@ void singlePlayerGame() {
 }
 
 void gamePlayer1(){
-    if(waitingPlayer2() != 0){
-      printf("FAILED\n" );
-    }else
-    printf("JA TA LA DENTRO TAMBEM\n" );
+    if(waitingPlayer2() != 0){ //Assures both players are connected
+      return ;
+    }
 
+    int ipc_status;
+    message msg;
+    uint32_t r;
+
+    uint32_t kbc_irq_set = getKBC_IRQ();
+    uint32_t timer_irq_set = getTIMER_IRQ();
+    uint32_t mouse_irq_set = getMOUSE_IRQ();
+    uint32_t rtc_irq_set = getRTC_IRQ();
+    uint32_t uart_irq_set = getUART_IRQ();
+
+    uint32_t charReceived = 'e'; //char random
+
+    uint8_t nbyte = 0; //numero de bytes do scancode
+    bool wait = false;
+
+    loadGameNumbers(); //Creates Bitmaps for all numbers
+
+    Bitmap* ball_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Bola.bmp");
+    Bitmap* aim_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Crosshair.bmp");
+    Bitmap* singlePlayerField_bmp = loadBitmap("/home/lcom/labs/proj/bmp/SinglePlayerField.bmp");
+    Bitmap* player1Right_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player1RightHand.bmp");
+    Bitmap* player1Left_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player1LeftHand.bmp");
+    Bitmap* player2Right_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player2RightHand.bmp");
+    Bitmap* player2Left_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player2LeftHand.bmp");
+
+    Sprite* ball = createSprite(ball_bmp,4*getHorResolution()/5,getVerResolution()/2,0,0);
+    Sprite* player1 = createSprite(player1Right_bmp, 20,getVerResolution()/2,0,0);
+    Sprite* player2 = createSprite(player2Right_bmp, getHorResolution()- player2Right_bmp->bitmapInfoHeader.width ,getVerResolution()/2,0,0);
+    Sprite* aim = createSprite(aim_bmp, getHorResolution()/2,getVerResolution()/2,0,0);
+    drawBitmap(singlePlayerField_bmp,0,0, ALIGN_LEFT);
+    drawSprite(player1);
+    drawSprite(player2);
+    drawSprite(ball);
+    drawSprite(aim);
+    doubleBuffCall();
+
+    update_date();
+    while (scanByte != ESC_CODE) {
+
+      if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+        printf("driver_receive failed with: %d\n", r);
+        continue;
+      }
+      if (is_ipc_notify(ipc_status)) {
+        switch (_ENDPOINT_P(msg.m_source)) {
+          case HARDWARE:
+          if (msg.m_notify.interrupts & rtc_irq_set) {
+            //TODO: Apagar ou nao?
+            update_date();
+          }
+          if (msg.m_notify.interrupts & kbc_irq_set) {
+            kbc_ih();
+            if (kbc_ih_error) {
+              kbc_ih_error = false;
+              continue;
+            }
+            isTwoByte(&wait, &nbyte);
+            if (wait == false) {
+              scancode_parse(scanByte, nbyte);
+            }
+            set_move(player1, nbyte,'0');
+
+            tickdelay(micros_to_ticks(DELAY_US));
+          }
+          if (msg.m_notify.interrupts & timer_irq_set) {
+            timer_int_handler();
+
+            if (counter_t % 60 == 0){
+              update_date();
+            }
+
+            if(counter_t % 1 == 0) { //redundante
+              if(ball->y <= 70 || ball->y > getVerResolution()|| ball->x < 1 || ball->x > getHorResolution()) {
+                if(!pointHandler(ball, aim)){
+                  scanByte = ESC_CODE;
+                }
+              }
+              if(player1->y < 412){ //Changes the racket hand
+                player1->bmp = player1Left_bmp;
+              }else {
+                player1->bmp = player1Right_bmp;
+              }
+              if(player2->y < 412){ //Changes the racket hand
+                player2->bmp = player2Left_bmp;
+              }else {
+                player2->bmp = player2Right_bmp;
+              }
+              ball->x += ball->xspeed;
+              ball->y += ball->yspeed;
+              movePlayer(player1, 0, (uint32_t) getHorResolution()/2);
+              movePlayer(player2,(uint32_t) getHorResolution()/2,(uint32_t) getHorResolution());
+
+              if(ball->colided) {
+                if (is_left_pressed()){
+                  shootBall(ball, aim);
+                }
+              }
+              drawBitmap(singlePlayerField_bmp,0, 0, ALIGN_LEFT);
+              drawSprite(player1);
+              drawSprite(player2);
+              drawSprite(aim);
+              drawSprite(ball);
+              printPoints();
+              print_date();
+              doubleBuffCall();
+            }
+
+          }
+          if (msg.m_notify.interrupts & mouse_irq_set) {
+            mouse_ih();
+
+            if (mouse_ih_error == true){
+              byteNumber = 0;
+              continue;
+            }
+
+            if(byteNumber == 3){
+              parse_packet();
+              move_aim (aim);
+              byteNumber = 0;
+            }
+          }
+          if(msg.m_notify.interrupts & uart_irq_set){
+            serialPort_handler(&charReceived);
+            set_move(player2, 1, charReceived);
+          }
+          break;
+          default:
+          break;
+        }
+      } else {
+
+      }
+    }
+    deleteNumbers();
+    deleteBitmap(ball_bmp);
+    deleteBitmap(aim_bmp);
+    deleteBitmap(singlePlayerField_bmp);
+    deleteBitmap(player1Right_bmp);
+    deleteBitmap(player1Left_bmp);
+    deleteBitmap(player2Right_bmp);
+    deleteBitmap(player2Left_bmp);
 
 }
 void gamePlayer2(){
-    if(waitingPlayer1() != 0){
-      printf("FAILED\n" );
+    if(waitingPlayer1() != 0){//Assures both players are connected
+      return ;
     }
-    else
-    
-    printf("JA TA LA DENTRO \n" );
 
+    int ipc_status;
+    message msg;
+    uint32_t r;
+    uint32_t kbc_irq_set = getKBC_IRQ();
+    uint32_t timer_irq_set = getTIMER_IRQ();
+    uint32_t mouse_irq_set = getMOUSE_IRQ();
+    uint32_t rtc_irq_set = getRTC_IRQ();
+    uint32_t uart_irq_set = getUART_IRQ();
 
+    uint32_t charReceived = 'e'; //char random
+
+    uint8_t nbyte = 0; //numero de bytes do scancode
+    bool wait = false;
+
+    loadGameNumbers(); //Creates Bitmaps for all numbers
+
+    Bitmap* ball_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Bola.bmp");
+    Bitmap* aim_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Crosshair.bmp");
+    Bitmap* singlePlayerField_bmp = loadBitmap("/home/lcom/labs/proj/bmp/SinglePlayerField.bmp");
+    Bitmap* player2Right_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player2RightHand.bmp");
+    Bitmap* player2Left_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player2LeftHand.bmp");
+    Bitmap* player1Right_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player1RightHand.bmp");
+    Bitmap* player1Left_bmp = loadBitmap("/home/lcom/labs/proj/bmp/Player1LeftHand.bmp");
+
+    Sprite* ball = createSprite(ball_bmp,4*getHorResolution()/5,getVerResolution()/2,0,0);
+    Sprite* player2 = createSprite(player2Right_bmp, getHorResolution()- player2Right_bmp->bitmapInfoHeader.width ,getVerResolution()/2,0,0);
+    Sprite* player1 = createSprite(player1Right_bmp, 20,getVerResolution()/2,0,0);
+    Sprite* aim = createSprite(aim_bmp, getHorResolution()/2,getVerResolution()/2,0,0);
+    drawBitmap(singlePlayerField_bmp,0,0, ALIGN_LEFT);
+    drawSprite(player2);
+    drawSprite(player1);
+    drawSprite(ball);
+    drawSprite(aim);
+    doubleBuffCall();
+
+    update_date();
+
+    while (scanByte != ESC_CODE) {
+      /* Get a request message. */
+      if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+        printf("driver_receive failed with: %d\n", r);
+        continue;
+      }
+      if (is_ipc_notify(ipc_status)) { /* received notification */
+        switch (_ENDPOINT_P(msg.m_source)) {
+          case HARDWARE: /* hardware interrupt notification */
+          if (msg.m_notify.interrupts & rtc_irq_set) {
+            //TODO: Apagar ou nao?
+            update_date();
+          }
+          if (msg.m_notify.interrupts & kbc_irq_set) { /* subscribed interrupt */
+            kbc_ih();
+            if (kbc_ih_error) {
+              kbc_ih_error = false;
+              continue;
+            }
+            isTwoByte(&wait, &nbyte);
+            if (wait == false) {
+              scancode_parse(scanByte, nbyte);
+            }
+            set_move(player2, nbyte,'0');
+
+            tickdelay(micros_to_ticks(DELAY_US));
+          }
+          if (msg.m_notify.interrupts & timer_irq_set) {
+            timer_int_handler();
+
+            if (counter_t % 60 == 0){
+              update_date();
+            }
+
+            if(counter_t % 1 == 0) { //redundante
+              if(ball->y <= 70 || ball->y > getVerResolution()|| ball->x < 1 || ball->x > getHorResolution()) {
+                if(!pointHandler(ball, aim)){
+                  scanByte = ESC_CODE;
+                }
+              }
+              if(player2->y < 412){ //Changes the racket hand
+                player2->bmp = player2Left_bmp;
+              }else {
+                player2->bmp = player2Right_bmp;
+              }
+              if(player1->y < 412){ //Changes the racket hand
+                player1->bmp = player1Left_bmp;
+              }else {
+                player1->bmp = player1Right_bmp;
+              }
+              ball->x += ball->xspeed;
+              ball->y += ball->yspeed;
+              movePlayer(player2,(uint32_t) getHorResolution()/2,(uint32_t) getHorResolution());
+              movePlayer(player1, 0, (uint32_t) getHorResolution()/2);
+              if(ball->colided) {
+                if (is_left_pressed()){
+                  shootBall(ball, aim);
+                }
+              }
+              drawBitmap(singlePlayerField_bmp,0, 0, ALIGN_LEFT);
+              drawSprite(player2);
+              drawSprite(player1);
+              drawSprite(aim);
+              drawSprite(ball);
+              printPoints();
+              print_date();
+              doubleBuffCall();
+            }
+          }
+          if (msg.m_notify.interrupts & mouse_irq_set) {
+            mouse_ih();
+
+            if (mouse_ih_error == true){
+              byteNumber = 0;
+              continue;
+            }
+            if(byteNumber == 3){
+              parse_packet();
+              move_aim (aim);
+              byteNumber = 0;
+            }
+          }
+          if(msg.m_notify.interrupts & uart_irq_set){
+            serialPort_handler(&charReceived);
+            set_move(player1, 1, charReceived);
+          }
+          break;
+          default:
+          break; /* no other notifications expected: do nothing */
+        }
+      } else { /* received a standard message, not a notification */
+        /* no standard messages expected: do nothing */
+      }
+    }
+    deleteNumbers();
+    deleteBitmap(ball_bmp);
+    deleteBitmap(aim_bmp);
+    deleteBitmap(singlePlayerField_bmp);
+    deleteBitmap(player2Right_bmp);
+    deleteBitmap(player2Left_bmp);
+    deleteBitmap(player1Right_bmp);
+    deleteBitmap(player1Left_bmp);
 }
--- proj/playerMovement.c
+++ proj/playerMovement.c
@@ -4,6 +4,7 @@
 #include "bitmap.h"
 #include "keyboard.h"
 #include "video_gr.h"
+#include "serial_port.h"
 #include <math.h>
 
 int playerMovSpeed = 7;
@@ -11,36 +12,67 @@ int ballMovSpeed = 10;
 static int aimx = 0;
 static int aimy = 0;
 
-void set_move(Sprite *sp, uint8_t nbyte) {
+//charReceived deve ser '0' quando é para mexer o player controlado pelo user
+// ou então é  w a s d i j k l consoante o char recebido no serial port
+void set_move(Sprite *sp, uint8_t nbyte, char adversaryMov) {
+  if(adversaryMov == '0'){
+    if((nbyte == 1 && scanByte==KEY_W) || (nbyte == 2 && scanByte == KEY_UP )) {
+      sp->mov.MOVE_UP = true;
+      write_THR('w');
+    }else if((nbyte == 1 && scanByte==KEY_S) || (nbyte == 2 && scanByte == KEY_DOWN)) {
+      sp->mov.MOVE_DOWN = true;
+      write_THR('s');
+    }else if((nbyte == 1 && scanByte==KEY_A) || (nbyte == 2 && scanByte == KEY_LEFT)) {
+      sp->mov.MOVE_LEFT = true;
+      write_THR('a');
+    }else if((nbyte == 1 && scanByte==KEY_D) || (nbyte == 2 && scanByte == KEY_RIGTH)) {
+      sp->mov.MOVE_RIGHT = true;
+      write_THR('d');
+    }else if((nbyte == 1 && scanByte==KEY_W_BREAK) || (nbyte == 2 && scanByte == KEY_UP_BREAK)) {
+      sp->mov.MOVE_UP = false;
+      write_THR('i');
+    }else if((nbyte == 1 && scanByte==KEY_S_BREAK) || (nbyte == 2 && scanByte == KEY_DOWN_BREAK)) {
+      sp->mov.MOVE_DOWN = false;
+      write_THR('k');
+    }else if((nbyte == 1 && scanByte==KEY_A_BREAK) || (nbyte == 2 && scanByte == KEY_LEFT_BREAK)) {
+      sp->mov.MOVE_LEFT = false;
+      write_THR('j');
+    }else if((nbyte == 1 && scanByte==KEY_D_BREAK) || (nbyte == 2 && scanByte == KEY_RIGTH_BREAK)) {
+      sp->mov.MOVE_RIGHT = false;
+      write_THR('l');
+    }
+  } else if( adversaryMov != '0') {
 
-  if((nbyte == 1 && scanByte==KEY_W) || (nbyte == 2 && scanByte == KEY_UP)) {
-    sp->mov.MOVE_UP = true;
+    if(adversaryMov == 'w'){
+      sp->mov.MOVE_UP = true;
 
-  }else if((nbyte == 1 && scanByte==KEY_S) || (nbyte == 2 && scanByte == KEY_DOWN)) {
-    sp->mov.MOVE_DOWN = true;
+    }else if(adversaryMov == 's') {
+      sp->mov.MOVE_DOWN = true;
 
-  }else if((nbyte == 1 && scanByte==KEY_A) || (nbyte == 2 && scanByte == KEY_LEFT)) {
-    sp->mov.MOVE_LEFT = true;
+    }else if(adversaryMov == 'a') {
+      sp->mov.MOVE_LEFT = true;
 
-  }else if((nbyte == 1 && scanByte==KEY_D) || (nbyte == 2 && scanByte == KEY_RIGTH)) {
-    sp->mov.MOVE_RIGHT = true;
+    }else if(adversaryMov == 'd') {
+      sp->mov.MOVE_RIGHT = true;
 
-  }else if((nbyte == 1 && scanByte==KEY_W_BREAK) || (nbyte == 2 && scanByte == KEY_UP_BREAK)) {
-    sp->mov.MOVE_UP = false;
+    }else if(adversaryMov == 'i') {
+      sp->mov.MOVE_UP = false;
 
-  }else if((nbyte == 1 && scanByte==KEY_S_BREAK) || (nbyte == 2 && scanByte == KEY_DOWN_BREAK)) {
-    sp->mov.MOVE_DOWN = false;
+    }else if(adversaryMov == 'k') {
+      sp->mov.MOVE_DOWN = false;
 
-  }else if((nbyte == 1 && scanByte==KEY_A_BREAK) || (nbyte == 2 && scanByte == KEY_LEFT_BREAK)) {
-    sp->mov.MOVE_LEFT = false;
+    }else if(adversaryMov == 'j') {
+      sp->mov.MOVE_LEFT = false;
 
-  }else if((nbyte == 1 && scanByte==KEY_D_BREAK) || (nbyte == 2 && scanByte == KEY_RIGTH_BREAK)) {
-    sp->mov.MOVE_RIGHT = false;
+    }else if(adversaryMov == 'l') {
+      sp->mov.MOVE_RIGHT = false;
+    }
   }
+
 
 }
 
-void movePlayer(Sprite* sp){
+void movePlayer(Sprite* sp, uint32_t xLeft, uint32_t xRight){
 
   if(sp->mov.MOVE_UP){
     sp->y -=playerMovSpeed;
@@ -58,14 +90,14 @@ void movePlayer(Sprite* sp){
 
   if(sp->mov.MOVE_LEFT){
     sp->x -=playerMovSpeed;
-    if(sp->x <= 0){
-      sp->x = 1;
+    if(sp->x <(int) xLeft){
+      sp->x = xLeft;
     }
   }
 
   if(sp->mov.MOVE_RIGHT){
     sp->x +=playerMovSpeed;
-    if(sp->x + sp->width >= (uint32_t)getHorResolution()/2){
+    if(sp->x + sp->width >= (unsigned int )xRight){
       sp->x -=playerMovSpeed;
     }
   }
@@ -78,24 +110,24 @@ void shootBall(Sprite* ball, Sprite* aim){
   int bally = ball->y;
   aimx = aim->x;
   aimy = aim->y;
-/*  printf("x %d\n",ballx );
+  /*  printf("x %d\n",ballx );
   printf("y %d\n",bally );
   printf("x %d\n",aimx );
   printf("y %d\n",aimy );*/
 
   double angle = atan2((double)(aimy - bally),(aimx - ballx));
   float l = angle;
- printf("a %f \n",l);
-char buffer[20];
-sprintf (buffer,  "a %f", l );
-printf("%s\n", buffer);
+  printf("a %f \n",l);
+  char buffer[20];
+  sprintf (buffer,  "a %f", l );
+  printf("%s\n", buffer);
 
-//  printf("t %d\n", ((double)(aimy - bally)/(aimx - ballx)));
+  //  printf("t %d\n", ((double)(aimy - bally)/(aimx - ballx)));
 
   ball->xspeed = ballMovSpeed * cos(angle);
   ball->yspeed = ballMovSpeed * sin(angle);
-//  printf("x %d\n",ball->xspeed );
-//  printf("y %d\n",ball->yspeed );
+  //  printf("x %d\n",ball->xspeed );
+  //  printf("y %d\n",ball->yspeed );
 }
 
 int getAimx(){
